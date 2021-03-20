1daa0f398a7b318933e73d03e920f7ff386a5fff
Dominus8 <ikaei162@mail.ru>
2021-03-20 22:47:05 +0300

    добавил выбор пути к базе в зависимости от ОС, дописал функционал тегирования постов и сортировку по тегам

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/ilarudy/PycharmProjects/flaskProject1/pypet.db'
+    from sys import platform
+    if platform == "linux" or platform == "linux2":
+        SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/ilarudy/PycharmProjects/flaskProject1/pypet.db'
+    elif platform == "darwin":
+        SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/ilarudy/PycharmProjects/flaskProject1/pypet.db'
+    elif platform == "win32":
+        SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/ilarudy/PycharmProjects/flaskProject1/pypet.db'
--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-from models import Post
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -17,3 +17,10 @@ def post_detail(slug):
     post = Post.query.filter(Post.slug == slug).first()
     tags = post.tags
     return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts.route('/rag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
