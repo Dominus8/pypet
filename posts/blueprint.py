b354ed6300eabccbd8f7d4fec922506a0990f50f
Dominus8 <ikaei162@mail.ru>
2021-03-21 22:53:49 +0300

    Доделана пагинация

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -22,7 +22,7 @@ def create_post():
             db.session.commit()
         except:
             print('база не смогла')
-        return redirect( url_for('posts.index'))
+        return redirect(url_for('posts.index'))
 
     form = PostForm()
     return render_template('posts/create_post.html', form=form)
@@ -31,11 +31,21 @@ def create_post():
 @posts.route('/')
 def index():
     q = request.args.get('q')
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     if q:
-        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))  # .all()
     else:
         posts = Post.query.order_by(Post.created.desc())
-    return render_template('posts/index.html', posts=posts)
+
+    pages = posts.paginate(page=page, per_page=5)
+
+    return render_template('posts/index.html', posts=posts, pages=pages)
 
 
 @posts.route('/<slug>')
